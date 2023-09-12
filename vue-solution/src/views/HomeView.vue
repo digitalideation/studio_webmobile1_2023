94d654a49ff10f4b3d5af558653927e1122be68b
Maria Husmann <maria.husmann@gmail.com>
2023-09-12 10:56:31 +0200

    added vue 3 solution

---
+++ vue-solution/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Commute from '../components/Commute.vue'
+import { reactive } from 'vue'
+import contentful from "@/modules/contentful.js"
+
+const commutes = reactive([]);
+
+(async () => {
+  commutes.push(...(await contentful.getCommutes()))
+}
+)()
+</script>
+
+<template>
+  <main>
+    <li v-for="commute in commutes" :key="commute.person">
+      <Commute :person="commute.fields.person" :start="commute.fields.start" :destination="commute.fields.destination" />
+    </li>
+  </main>
+</template>
