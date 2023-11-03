6281f769b78d13bb1c92ce9e7e695a545584f7cd
Maria Husmann <maria.husmann@gmail.com>
2023-11-03 10:07:16 +0100

    added performance samples

---
+++ performance/vue-solution/src/views/AboutView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+const ready = ref(0);
+function getReady() {
+  for (let index = 0; index < 10000000; index++) {
+    ready.value++;
+  }
+}
+
+setTimeout(getReady, 2000);
+
+</script>
+
+<template>
+  <div class="about">
+    <h1 v-if="ready > 9000000">This is an about page with a very non-descriptive text</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
